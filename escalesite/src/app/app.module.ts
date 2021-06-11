a3185f5274fa3ec37080b3b0e3554a9c05b8f37a
TomasP <perezgantomas@gmail.com>
2021-06-11 07:56:23 -0400

    new components added

--- escalesite/src/app/app.module.ts
+++ escalesite/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
