59b282572ea92764f796ebc50c2828d3bcbba30e
TomasP <perezgantomas@gmail.com>
2021-06-16 08:06:56 -0400

    adding form required input logic

--- escalesite/src/app/app.component.ts
+++ escalesite/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'escalesite';
+
+  scroll() {
+    document.querySelector('#contact-page').scrollIntoView({ behavior: 'smooth'});
+ }
 }
