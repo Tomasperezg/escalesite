5fd45dd6809d7a7a420f17cf54eb0b33589d974c
TomasP <perezgantomas@gmail.com>
2021-06-16 06:55:18 -0400

    Updates to Contact form

--- escalesite/src/app/contact/contact.component.ts
+++ escalesite/src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Form } from '../form'
+
 
 @Component({
   selector: 'app-contact',
@@ -9,6 +11,16 @@ export class ContactComponent implements OnInit {
 
   constructor() { }
 
+  subjects = ['--Selectt Message Topic--', '', 'Consult (Free)']
+
+  model = new Form('Tom', 123456789, 'some@some.com',  this.subjects[0], 'This is a sample Message')
+
+  submit = false;
+  
+  onSubmit(){
+    this.submit = true;
+  }
+  
   ngOnInit() {
   }
 
---
+++ escalesite/src/app/form.ts
@@ -0,0 +1,10 @@
+export class Form {
+
+    constructor(
+        public name: string,
+        public number: number,
+        public email: string,
+        public subject: string,
+        public message: string
+    ){}
+}
