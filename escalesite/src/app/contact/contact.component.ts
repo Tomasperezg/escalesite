7a391acf4b968efdfcdb70ea0cb096b5c15a2cf7
TomasP <perezgantomas@gmail.com>
2021-06-17 08:25:47 -0400

    form template logic complete

--- escalesite/src/app/contact/contact.component.ts
+++ escalesite/src/app/contact/contact.component.ts
@@ -11,14 +11,15 @@ export class ContactComponent implements OnInit {
 
   constructor() { }
 
-  subjects = ['--Selectt Message Topic--', '', 'Consult (Free)']
+  subjects = ['--Select Message Topic--', '', 'Consult (Free)']
 
   model = new Form('Tom', 123456789, 'some@some.com',  this.subjects[0], 'This is a sample Message')
 
-  submit = false;
+  submitted = false;
   
   onSubmit(){
-    this.submit = true;
+    this.submitted = true;
+    console.log('Message sent')
   }
   
   ngOnInit() {
